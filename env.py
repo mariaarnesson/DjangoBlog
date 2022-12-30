302502f04fdf4f5e5b2b8fb5d9d1964f87b1970e
mariaarnesson <maria_arnesson@outlook.com>
2022-12-30 21:08:24 +0000

    deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://jmhikpzn:jAnrLfaw1vyQNerKm5s1WmD95YuSh3Ge@mel.db.elephantsql.com/jmhikpzn"
+os.environ["SECRET_KEY"] = "randomSecretKey1881!!"
+os.environ["CLOUDINARY_URL"] = "CLOUDINARY_URL=cloudinary://227949597639582:ssX8xgBV9dzS3L5H8Nh8Vhr0kfg@dun5p7nsx"
